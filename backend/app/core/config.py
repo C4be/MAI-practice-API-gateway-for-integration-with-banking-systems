5510b91412b7d8b8aa76850f94d6aee6008fa0a6
SuperCube <dmitrycube@yandex.ru>
2025-03-05 00:05:19 +0300

    try docker so sad

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,8 @@
 # test на локалхосте
-DATABASE_URL = 'postgresql://fast_api:root@localhost:5445/bankDB'
-# DATABASE_MONGO_URL = 'mongodb://fast_api:root@localhost:27018/fileStorageDB'
+# DATABASE_URL = 'postgresql://fast_api:root@localhost:5445/bankDB'
+# MONGO_DB_NAME = 'files'
+# DATABASE_MONGO_URL = 'mongodb://fast_api:root@localhost:27018/fileStorageDB?authSource=admin'
+
+DATABASE_URL = 'postgresql://fast_api:root@app_network:5432/bankDB'
 MONGO_DB_NAME = 'files'
-DATABASE_MONGO_URL = 'mongodb://fast_api:root@localhost:27018/fileStorageDB?authSource=admin'
+DATABASE_MONGO_URL = 'mongodb://fast_api:root@app_network:27017/fileStorageDB?authSource=admin'
