463d8c118d2844587709d95c66385162573c7849
C4be <dmitryc4be@gmail.com>
2025-03-05 11:39:37 +0300

    Docker run success

--- README.md
+++ README.md
@@ -264,7 +264,9 @@
 sudo apt install python3-dev libpq-dev gcc
 ```
 
-
+1. Пишем в `requirements.txt` все необходимые зависимости
+2. Устанавливаем их `pip install -r requirements.txt` 
+3. Запуск сервера `uvicorn app.main:app --reload --host 0.0.0.0 --port 8080` в режиме разработки
 
 
 
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,6 +3,6 @@
 # MONGO_DB_NAME = 'files'
 # DATABASE_MONGO_URL = 'mongodb://fast_api:root@localhost:27018/fileStorageDB?authSource=admin'
 
-DATABASE_URL = 'postgresql://fast_api:root@app_network:5432/bankDB'
-MONGO_DB_NAME = 'files'
-DATABASE_MONGO_URL = 'mongodb://fast_api:root@app_network:27017/fileStorageDB?authSource=admin'
+DATABASE_URL = "postgresql://fast_api:root@postgres:5432/bankDB"
+MONGO_DB_NAME = "files"
+DATABASE_MONGO_URL = "mongodb://fast_api:root@mongo:27017/fileStorageDB?authSource=admin"
